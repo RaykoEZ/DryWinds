b5928d1c2ce4de5b108b6342f4374b34f1c96bf2
RaykoEZ <onion55zh@gmail.com>
2022-01-07 01:17:08 +0000

    Implementing base emotion handling

---
+++ Assets/src/Ai/Heuristics/Emotion/EmotionHandler.cs
@@ -0,0 +1,43 @@
+﻿namespace Curry.Ai
+{
+    public abstract class EmotionHandler
+    {
+        public virtual AiEmotion Default { get { return m_default; } }
+        public virtual AiEmotion Current { get { return m_current; } }
+        protected AiEmotion m_default;
+        protected AiEmotion m_current;
+        public virtual void Init() 
+        {
+            m_default = new AiEmotion();
+            m_current = new AiEmotion();
+        }
+        public abstract void OnTakeDamage();
+        public abstract void OnAllyDefeated();
+        public abstract void OnThreatDetected();
+        public abstract void OnHelped();
+    }
+
+    public class BaseEmotionHandler : EmotionHandler
+    {
+        public override void OnAllyDefeated()
+        {
+            m_current += new AiEmotion(0f, 0.5f);
+        }
+
+        public override void OnTakeDamage()
+        {
+            m_current += new AiEmotion(0.5f, 1f);
+        }
+
+        public override void OnThreatDetected() 
+        {
+            m_current += new AiEmotion(0f, 0.5f);
+        }
+
+        public override void OnHelped()
+        {
+            m_current -= new AiEmotion(1f, 0.5f);
+        }
+    }
+
+}
