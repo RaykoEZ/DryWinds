7f98500df9cbb6664976837ce5c8d84293a8d3b3
RaykoEZ <onion55zh@gmail.com>
2022-11-01 02:41:08 +0000

    Working on animator implementation for enemy0

--- Assets/src/Explore/Npc/TacticalEnemy.cs
+++ Assets/src/Explore/Npc/TacticalEnemy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Curry.Explore
@@ -18,27 +19,28 @@ namespace Curry.Explore
         {
             m_current = m_initStats;    
         }
-
         public virtual void Reveal() 
         {
             m_current.Visibility = TacticalVisibility.Visible;
-            m_anim?.SetTrigger("reveal");
-            m_anim?.SetBool("hidden", false);
-
+            m_anim.SetTrigger("reveal");
         }
         public virtual void Hide() 
         {
             m_current.Visibility = TacticalVisibility.Hidden;
-            m_anim?.SetTrigger("hide");
-            m_anim?.SetBool("hidden", true);
+            m_anim.SetTrigger("hide");
         }
+
         public virtual void TakeHit() 
         {
             Debug.Log("Ahh, me ded");
             m_anim?.SetTrigger("takeHit");
             m_anim?.SetBool("defeat", true);
         }
+        protected virtual void OnCombat() 
+        {
+            m_anim?.SetBool("engage", true);
 
+        }
         public virtual void Affect(Func<TacticalStats, TacticalStats> effect)
         {
             if (effect == null) return;
