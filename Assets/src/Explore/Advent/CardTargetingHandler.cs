ddc8a176b319b26f86f0a9de87dc662d52388dde
RaykoEZ <onion55zh@gmail.com>
2022-12-05 00:05:03 +0000

    Implemented target line guide card effects requiring target selection

--- Assets/src/Explore/Advent/CardTargetingHandler.cs
+++ Assets/src/Explore/Advent/CardTargetingHandler.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
-
+using UnityEngine.InputSystem;
 namespace Curry.Explore
 {
-    public class DirectionLineHandler: MonoBehaviour 
+    public class CardTargetingHandler: MonoBehaviour 
     {
         [SerializeField] LineRenderer m_line = default;
-        public void BeginLine(Transform origin) 
+        public void BeginLine(Vector3 origin) 
         {
             m_line.positionCount = 2;
-            m_line.SetPosition(0, origin.position);
+            m_line.SetPosition(0, origin);
 
         }
         public void UpdateLine(PointerEventData eventData)
         {
-            m_line.SetPosition(1, eventData.position);
+            Vector2 screen = Pointer.current.position.ReadValue();
+            Vector2 world = Camera.main.ScreenToWorldPoint(screen);
+            m_line.SetPosition(1, world);
         }
         public void Clear() 
         {
--- Assets/src/Explore/Advent/TileTargetCard.cs
+++ Assets/src/Explore/Advent/TileTargetCard.cs
@@ -7,12 +7,12 @@ namespace Curry.Explore
     // A card that needs to drag and select a tile/position to activate.
     public class TileTargetCard : DraggableCard
     {
-        [SerializeField] DirectionLineHandler m_line = default;
+        [SerializeField] CardTargetingHandler m_line = default;
         [SerializeField] CurryGameEventTrigger m_onCardTargetTile = default;
         // Make line and change cursor icon to target a tile
         public override void OnBeginDrag(PointerEventData eventData)
         {
-            m_line?.BeginLine(transform);
+            m_line?.BeginLine(transform.position);
             base.OnBeginDrag(eventData);
             EventInfo info = new EventInfo();
         }
@@ -20,7 +20,6 @@ namespace Curry.Explore
         public override void OnDrag(PointerEventData eventData)
         {
             m_line?.UpdateLine(eventData);
-            base.OnDrag(eventData);
         }
         // Check if there is a valid target selected, cancel card activation if not
         public override void OnEndDrag(PointerEventData eventData)
