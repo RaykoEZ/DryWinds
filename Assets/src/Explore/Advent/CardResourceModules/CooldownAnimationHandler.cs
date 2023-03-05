03d77a969ab606bfd078283fec4340d2e278e2c8
RaykoEZ <onion55zh@gmail.com>
2023-03-05 00:07:43 +0000

    implementing card cooldown mechanics

---
+++ Assets/src/Explore/Advent/CardResourceModules/CooldownAnimationHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Curry.Explore
+{
+    [RequireComponent(typeof(Animator))]
+    public class CooldownAnimationHandler : MonoBehaviour 
+    { 
+        public void OnCooldown(bool isOnCooldown = true) 
+        {
+            GetComponent<Animator>()?.SetBool("cooldown", isOnCooldown);
+        }
+    }
+}
--- Assets/src/Explore/Advent/CardResourceModules/CooldownModule.cs
+++ Assets/src/Explore/Advent/CardResourceModules/CooldownModule.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 namespace Curry.Explore
 {
     [Serializable]
+    [RequireComponent(typeof(CooldownAnimationHandler))]
     public class CooldownModule : CardResourceModule, ICooldown
     {
         [SerializeField] protected int m_cooldownTime = default;
         [SerializeField] protected bool m_isInitiallyOnCooldown = default;
+        protected CooldownAnimationHandler CooldownAnim => GetComponent<CooldownAnimationHandler>();
         protected int m_current = 0;
         public bool IsOnCooldown { get; protected set; }
         public int CooldownTime { get => m_cooldownTime; set => m_cooldownTime = value; }
@@ -16,17 +18,24 @@ namespace Curry.Explore
         public override void Init()
         {
             IsOnCooldown = m_isInitiallyOnCooldown;
+            AnimateCooldown();
             m_current = 0;
         }
         public void TrggerCooldown()
         {
             m_current = m_cooldownTime;
             IsOnCooldown = true;
+            AnimateCooldown();
         }
         public void Tick(int dt, out bool isOnCoolDown)
         {
             m_current -= dt;
             isOnCoolDown = m_current <= 0;
+            AnimateCooldown();
+        }
+        protected virtual void AnimateCooldown() 
+        {
+            CooldownAnim?.OnCooldown(IsOnCooldown);
         }
     }
 }
