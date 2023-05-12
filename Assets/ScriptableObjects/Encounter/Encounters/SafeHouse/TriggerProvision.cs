06fa75964bf1a5961c923514ef37f550fb3232be
RaykoEZ <onion55zh@gmail.com>
2023-05-12 23:58:20 +0100

    Adding backdrop for encounter: mirage, shelter

--- Assets/ScriptableObjects/Encounter/Encounters/SafeHouse/TriggerProvision.cs
+++ Assets/ScriptableObjects/Encounter/Encounters/SafeHouse/TriggerProvision.cs
@@ -1,3 +1,4 @@
+using Curry.Events;
 using System;
 using UnityEngine;
 namespace Curry.Explore
@@ -5,14 +6,14 @@ namespace Curry.Explore
     [Serializable]
     public class TriggerProvision : PropertyAttribute
     {
-        
+        [SerializeField] CurryGameEventTrigger m_provisionEvent = default;
         public void ApplyEffect(ICharacter target, ICharacter user)
         {
-            
+            m_provisionEvent?.TriggerEvent(new EventInfo());
         }
         public void ApplyEffect(ICharacter target)
         {
-            
+            m_provisionEvent?.TriggerEvent(new EventInfo());
         }
     }
 }
--- Assets/ScriptableObjects/Encounter/Encounters/SafeHouse/TriggerProvision_EffectResource.cs
+++ Assets/ScriptableObjects/Encounter/Encounters/SafeHouse/TriggerProvision_EffectResource.cs
@@ -6,8 +6,7 @@ namespace Curry.Explore
     [Serializable]
     [CreateAssetMenu(fileName = "TriggerProvision_", menuName = "Curry/Effects/TriggerProvision", order = 1)]
     public class TriggerProvision_EffectResource : BaseEffectResource
-    {
-        
+    {       
         [SerializeField] TriggerProvision m_effect = default;
         public TriggerProvision Effect => m_effect;
         public override void Activate(GameStateContext context)
