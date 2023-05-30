3cbe2853b58ef104061a7332a87ef481aaa82760
RaykoEZ <onion55zh@gmail.com>
2023-05-30 22:37:39 +0100

    Added scene transition animation and scene loading does not freeze to activate scene on load.

--- Assets/src/UI/LevelLoader.cs
+++ Assets/src/UI/LevelLoader.cs
@@ -6,6 +6,7 @@ namespace Assets.src.UI
 {
     public class LevelLoader : MonoBehaviour
     {
+        [SerializeField] Animator m_transition = default;
         AsyncOperation m_loadSceneOp;
         public void LoadScene(int sceneIndex) 
         {
@@ -13,8 +14,15 @@ namespace Assets.src.UI
         }
         IEnumerator LoadLevel_Internal(int sceneIndex) 
         {
+            // start scene loading
             m_loadSceneOp = SceneManager.LoadSceneAsync(sceneIndex);
-            yield return null;           
+            m_loadSceneOp.allowSceneActivation = false;
+            // play transition animaton
+            m_transition?.SetTrigger("transition");
+            yield return new WaitForSeconds(1f);
+            // transition to new scene when loading and animation are done
+            yield return new WaitUntil(() => m_loadSceneOp.progress >= 0.9f);
+            m_loadSceneOp.allowSceneActivation = true;
         }
     }
 }
